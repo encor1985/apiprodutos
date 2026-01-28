cf7a208905dd52ab87f920338d33e1adabb012b8
Encor Estudios E Tecnologia <encorestudiostec@gmail.com>
2026-01-28 20:08:32 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { pool } from "./db.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+/*lista todos*/
 
 app.get("/cozinha", async (req, res) => {
   const result = await pool.query("SELECT * FROM cozinha ORDER BY prato");
@@ -15,4 +16,22 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando");
 });
+/*busca avançada*/
+app.get("/cozinha/busca",async(req,res)=>{
+  const{
+    prato="",
+    quantidade=""
+  }=req.query;
+  const r=await pool.query(
+    `SELECT * FROM cozinha 
+    WHERE prato ILIKE $1
+     AND quantidade ILIKE $2
+    ORDER BY id`,
+    [
+      `%${prato}%`,
+      `%${quantidade}%`
+    ]
+  );
+  res.json(r.rows);
+})
 
